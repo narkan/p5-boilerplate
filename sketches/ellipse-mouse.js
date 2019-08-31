bcd7ad11817673e16d4e8911d6ae6c70fa815c23
Toby Cosh <toby@narkan.co.uk>
2019-08-31 20:49:49 +0100

    With examples

---
+++ sketches/ellipse-mouse.js
@@ -0,0 +1,25 @@
+let img;
+
+function preload() {
+    img = loadImage('./img/bg.jpg'); // bg.jpg courtesy rawpixel.com
+}
+
+function setup() {
+    let myCanvas = createCanvas(600, 400);
+    myCanvas.parent('p5-wrap');
+
+    drawingContext.shadowOffsetX = 5;
+    drawingContext.shadowOffsetY = -5;
+    drawingContext.shadowBlur = 10;
+    drawingContext.shadowColor = "gray";
+    background(200);
+}
+
+function draw() {
+    if (mouseIsPressed) {
+        fill(0);
+    } else {
+        fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
+}
